888d21efead0c811401fc1584a81a8126a261254
borrus <55027862+borrus@users.noreply.github.com>
2021-04-01 23:46:21 +0300

    added LZW

---
+++ task 2.1 sem2/task 2.1 sem2/Bor.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_2._1_sem2
+{
+    class Bor
+    {
+        private class Node
+        {
+            public List<byte> key;
+            public byte value;
+            public List<Node> children;
+            public Node parent;
+
+            public Node(List<byte> key, byte value, Node parent = null)
+            {
+                this.key = Utilities.ListCopy(key);
+                this.value = value;
+                this.children = new List<Node>();
+                this.parent = parent;
+            }
+
+            public Node GetNodeByChain(List<byte> chain)
+            {
+                if (chain.Count == 0)
+                {
+                    return this;
+                }
+
+                for (int i = 0; i < this.children.Count; ++i)
+                {
+                    if (this.children[i].value == chain[0])
+                    {
+                        List<byte> newChain = Utilities.ListCopy(chain);
+                        newChain.RemoveAt(0);
+                        return this.children[i].GetNodeByChain(newChain);
+                    }
+                }
+
+                return null;
+            }
+
+            public Node GetNodeByKey(List<byte> key)
+            {
+                if (Utilities.ListByteEqual(this.key, key))
+                {
+                    return this;
+                }
+
+                for (int i = 0; i < this.children.Count; ++i)
+                {
+                    Node node = this.children[i].GetNodeByKey(key);
+                    if (node != null)
+                    {
+                        return node.GetNodeByKey(key);
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        private Node root;
+
+        private int CodeSize;
+
+        public List<byte> LastCode;
+
+        private void IncreaseCode()
+        {
+            for (int i = 0; i < this.CodeSize; i++)
+            {
+                if (LastCode[i] < 255)
+                {
+                    ++LastCode[i];
+                    return;
+                }
+            }
+        }
+
+        public Bor(int CodeSize)
+        {
+            this.LastCode = new List<byte>(CodeSize);
+            for (int i = 0; i < LastCode.Capacity; i++)
+            {
+                this.LastCode.Add(0);
+            }
+            this.CodeSize = CodeSize;
+            this.root = new Node(new List<byte>(0), 0);
+            this.root.children = new List<Node>(256);
+
+            for (int i = 0; i < 255; i++)
+            {
+                this.root.children.Add(new Node(LastCode, (byte)i));
+                this.IncreaseCode();
+            }
+
+            this.root.children.Add(new Node(LastCode, (byte)255));
+        }
+
+        public bool ContainsValue(List<byte> value)
+        {
+            return this.GetNodeByChain(value) != null;
+        }
+
+        public bool ContainsKey(List<byte> key)
+        {
+            return this.GetValueByKey(key) != null;
+        }
+
+        public List<byte> GetKeyByValue(List<byte> value)
+        {
+            if (!this.ContainsValue(value))
+            {
+                return null;
+            }
+
+            return this.GetNodeByChain(value).key;
+        }
+
+        public List<byte> GetValueByKey(List<byte> key)
+        {
+            Node temp = this.GetNodeByKey(key);
+
+            if (temp == null)
+            {
+                return null;
+            }
+
+            List<byte> chain = new List<byte>();
+
+            while (temp != null)
+            {
+                chain.Add(temp.value);
+                temp = temp.parent;
+            }
+
+            chain.Reverse();
+
+            return chain;
+        }
+
+        public void Add(List<byte> chain)
+        {
+            if (this.GetNodeByChain(chain) != null)
+            {
+                return;
+            }
+
+            byte lastSymbol = chain[chain.Count - 1];
+            chain.RemoveAt(chain.Count - 1);
+
+            Node parent = GetNodeByChain(chain);
+
+            chain.Add(lastSymbol);
+
+            if (parent == null)
+            {
+                return;
+            }
+
+            IncreaseCode();
+
+            Node nodeToAdd = new Node(LastCode, lastSymbol, parent);
+            parent.children.Add(nodeToAdd);
+        }
+        private Node GetNodeByChain(List<byte> chain)
+        {
+            for (int i = 0; i < this.root.children.Count; ++i)
+            {
+                if (this.root.children[i].value == chain[0])
+                {
+                    List<byte> newChain = Utilities.ListCopy(chain);
+                    newChain.RemoveAt(0);
+                    return this.root.children[i].GetNodeByChain(newChain);
+                }
+            }
+
+            return null;
+        }
+
+        private Node GetNodeByKey(List<byte> key)
+        {
+            for (int i = 0; i < this.root.children.Count; ++i)
+            {
+                Node node = this.root.children[i].GetNodeByKey(key);
+
+                if (node != null)
+                {
+                    return node.GetNodeByKey(key);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ task 2.1 sem2/task 2.1 sem2/LZW.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace task_2._1_sem2
+{
+    class LZW
+    {
+        private Bor codeTable;
+
+        private int codeSize;
+
+        private List<byte> maxCode;
+
+        private List<byte> clearCode;
+
+        public LZW(int CodeSize)
+        {
+            this.codeSize = CodeSize;
+
+            this.maxCode = new List<byte>(this.codeSize);
+            for (int i = 0; i < this.codeSize; ++i)
+            {
+                this.maxCode.Add((byte)255);
+            }
+            this.maxCode[this.codeSize - 1] = (byte)254;
+
+            this.clearCode = Utilities.ListCopy(this.maxCode);
+            this.clearCode[this.codeSize - 1] = (byte)255;
+
+            this.codeTable = new Bor(this.codeSize);
+        }
+
+        public void LZWEncoding(string inputFilePath)
+        {
+            FileStream input = File.OpenRead(inputFilePath);
+            BinaryReader reader = new BinaryReader(input);
+            string output = "encoded.txt";
+            File.Delete(output);
+
+            List<byte> chain = new List<byte>();
+
+            while (input.Position != input.Length)
+            {
+                byte symbol = reader.ReadByte();
+
+                if (codeTable.ContainsValue(Utilities.ListPlusSymbol(chain, symbol)))
+                {
+                    chain.Add(symbol);
+                }
+                else
+                {
+                    Utilities.WriteByteListToFile(codeTable.GetKeyByValue(chain), output);
+
+                    codeTable.Add(Utilities.ListPlusSymbol(chain, symbol));
+
+                    chain.Clear();
+
+                    chain.Add(symbol);
+
+                    if (Utilities.ListByteEqual(this.codeTable.LastCode, this.maxCode))
+                    {
+                        Utilities.WriteByteListToFile(codeTable.GetKeyByValue(chain), output);
+                        Utilities.WriteByteListToFile(this.clearCode, output);
+                        this.codeTable = new Bor(this.codeSize);
+
+                        chain.Clear();
+                    }
+                }
+            }
+
+            Utilities.WriteByteListToFile(codeTable.GetKeyByValue(chain), output);
+
+            reader.Close();
+            input.Close();
+        }
+
+        public void LZWDecoding(string inputFilePath)
+        {
+            FileStream input = File.OpenRead(inputFilePath);
+            BinaryReader reader = new BinaryReader(input);
+            string output = "decoded.txt";
+            File.Delete(output);
+
+            List<byte> previous = new List<byte>();
+
+            while (input.Position != input.Length)
+            {
+                List<byte> next = Utilities.ReadLisOfByteFromFile(reader, this.codeSize);
+
+                if (Utilities.ListByteEqual(this.clearCode, next))
+                {
+                    this.codeTable = new Bor(this.codeSize);
+
+                    if (input.Position == input.Length)
+                    {
+                        break;
+                    }
+
+                    next = Utilities.ReadLisOfByteFromFile(reader, this.codeSize);
+                    Utilities.WriteByteListToFile(codeTable.GetValueByKey(next), output);
+                    previous = Utilities.ListCopy(next);
+                }
+                else
+                {
+                    if (codeTable.ContainsKey(next))
+                    {
+                        Utilities.WriteByteListToFile(codeTable.GetValueByKey(next), output);
+
+                        List<byte> chain1 = codeTable.GetValueByKey(previous);
+                        List<byte> chain2 = codeTable.GetValueByKey(next);
+                        codeTable.Add(Utilities.ListPlusSymbol(chain1, chain2[0]));
+                    }
+                    else
+                    {
+                        List<byte> chain = Utilities.ListPlusSymbol(previous, previous[0]);
+                        Utilities.WriteByteListToFile(chain, output);
+                        codeTable.Add(chain);
+                    }
+
+                    previous = Utilities.ListCopy(next);
+                }
+            }
+
+            reader.Close();
+            input.Close();
+        }
+    }
+}
+
