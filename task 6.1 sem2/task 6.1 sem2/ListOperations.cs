0954769b2f47e1d495c595a86c0c36d535b9e614
borrus <55027862+borrus@users.noreply.github.com>
2021-06-04 21:01:32 +0300

    added task 6.1

---
+++ task 6.1 sem2/task 6.1 sem2/ListOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_6._1_sem2
+{
+    public static class ListOperations
+    {
+        public static List<T2> Map<T1, T2>(List<T1> list, Func<T1, T2> function)
+        {
+            List<T2> newList = new List<T2>(); 
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                newList.Add(function(list[i]));
+            }
+
+            return newList;
+        }
+
+        public static List<T> Filter<T>(List<T> list, Func<T, bool> function)
+        {
+            List<T> newList = new List<T>();
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (function(list[i]))
+                {
+                    newList.Add(list[i]);
+                }
+            }
+
+            return newList;
+        }
+
+        public static T2 Fold<T1, T2>(List<T1> list, T2 initialValue, Func<T2, T1, T2> function)
+        {
+            T2 result = initialValue;
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                result = function(result, list[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ task 6.1 sem2/task 6.1 sem2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_6._1_sem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
